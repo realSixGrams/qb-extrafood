aa6cf44a5aac775ade922726ebc9ad8c2c258f0d
realSixGrams <96337767+realSixGrams@users.noreply.github.com>
2021-12-18 15:03:47 -0500

    Add files via upload

---
+++ New QBCore Foods/New QBCore Foods/FuturePlans.lua
@@ -0,0 +1,8 @@
+-- Require Images for Missing Item PNGs
+-- Four new Drinks
+
+-- Second Set of Drinks/Foods(20 Each)
+-- Implement Seconday Benefits like Speed, and Strength
+
+-- Third Set of Drinks/Foods(30 Each)
+-- Implement Affects & Durations within Foods/Drinks(Similar to High/Drunk)
---
+++ New QBCore Foods/New QBCore Foods/qbsmallresourcesconfig.lua
@@ -0,0 +1,23 @@
+-- Add to QBCore/qb-smallresources/config.lua
+
+-- Add under "ConsumeablesEat"
+-- Make sure to add the Spaces to correct the Format
+["sushirolls"] = math.random(20, 32), -- 1
+["pizzaslice"] = math.random(12, 16), -- 2
+["cookie"] = math.random(5, 10), -- 3
+["muffin"] = math.random(5, 10), -- 4
+["fries"] = math.random(10, 18), -- 5
+["donut"] = math.random(8, 26), -- 6
+["icecream"] = math.random(5, 12), -- 7
+["cupcake"] = math.random(7, 18), -- 8
+["gum"] = math.random(3, 8), -- 9
+["ramen"] = math.random(3, 15), -- 10
+
+-- Add under "ConsumeablesDrink"
+-- Make sure to add the Spaces to correct the Format
+["sprite"] = math.random(35, 54), -- 1
+["pepsi"] = math.random(35, 54), -- 2
+["mtndew"] = math.random(35, 54), -- 3
+["lemonade"] = math.random(35, 54), -- 4
+["icedtea"] = math.random(35, 54), -- 5
+["milk"] = math.random(35, 54), -- 6
