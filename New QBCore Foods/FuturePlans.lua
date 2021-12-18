94e77fd04c8ae029a57cb811d9247a180fb32d31
realSixGrams <96337767+realSixGrams@users.noreply.github.com>
2021-12-18 07:38:09 -0500

    Update FuturePlans.lua

--- New QBCore Foods/FuturePlans.lua
+++ New QBCore Foods/FuturePlans.lua
@@ -2,7 +2,7 @@
 -- Four new Drinks
 
 -- Second Set of Drinks/Foods(20 Each)
--- Implement Seconday Benefits like Speed, and Strength
+-- Implement Seconday Benefits like Speed, and Strength "How To's"
 
 -- Third Set of Drinks/Foods(30 Each)
--- Implement Affects & Durations within Foods/Drinks(Similar to High/Drunk)
+-- Implement Affects & Durations "How To's" within Foods/Drinks(Similar to High/Drunk)
