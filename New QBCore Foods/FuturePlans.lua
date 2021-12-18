e4119b789db4f76ab901999d072a45fec9d24fba
realSixGrams <96337767+realSixGrams@users.noreply.github.com>
2021-12-18 15:06:18 -0500

    Delete FuturePlans.lua

--- New QBCore Foods/FuturePlans.lua
+++
@@ -1,8 +0,0 @@
--- Require Images for Missing Item PNGs
--- Four new Drinks
-
--- Second Set of Drinks/Foods(20 Each)
--- Implement Seconday Benefits like Speed, and Strength "How To's"
-
--- Third Set of Drinks/Foods(30 Each)
--- Implement Affects & Durations "How To's" within Foods/Drinks(Similar to being High/Drunk)
